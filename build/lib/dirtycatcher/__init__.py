8ecef2d0cb718c84af502dec173a7b24f9eec451
Christof Damian <christof@damian.net>
2025-07-08 17:58:37 +0200

    remove build

--- build/lib/dirtycatcher/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Dirtycatcher: A podcast downloader that uses dirtyget configuration format.
-"""
-
-from .core import PodcastDownloader
-
-__version__ = "0.1.0"
-__all__ = ["PodcastDownloader"]
